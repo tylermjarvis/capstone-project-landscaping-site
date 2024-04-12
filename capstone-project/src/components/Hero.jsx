a61d817c0a99c7d29f0c4ab1d3d4e7309303e8fc
Tyler Jarvis <tyler.jarvis@developersinstitute.co.nz>
2024-04-12 15:12:27 +1200

    Performance changes

--- capstone-project/src/components/Hero.jsx
+++ capstone-project/src/components/Hero.jsx
@@ -14,10 +14,14 @@ const Hero = () => {
     <section id="hero">
       <div className="hero" style={heroBackground}>
         <div className="caption text-center">
-          <p className="caption-1">Elevate Your Outdoor Space With</p>
-          <p className="caption-2"> Expert Landscaping Services.</p>
-          <p className="caption-3">From Lush Green Gardens to Unique Paving</p>
-          <p className="caption-4">We&apos;ve Got You Covered!!</p>
+          <p className="caption-1">
+            Elevate Your Outdoor Space With
+            <br />
+            Expert Landscaping Services.
+            <br />
+            From Lush Green Gardens to Unique Paving
+          </p>
+          <p className="caption-2">We&apos;ve Got You Covered!!</p>
         </div>
       </div>
     </section>
